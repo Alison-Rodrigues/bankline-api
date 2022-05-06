a28e03bb67de3bed5bc55d54a07a38d2047f536e
Alison Rodrigues <alison.si1302@gmail.com>
2022-05-05 23:43:03 -0300

    Reinicialização

--- src/main/java/diosantanderbankline/banklineapi/BanklineApiApplication.java
+++ src/main/java/diosantanderbankline/banklineapi/BanklineApiApplication.java
@@ -1,4 +1,4 @@
-package diosantander.banklineapi;
+package diosantanderbankline.banklineapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/diosantanderbankline/banklineapi/BanklineApiApplicationTests.java
+++ src/test/java/diosantanderbankline/banklineapi/BanklineApiApplicationTests.java
@@ -1,4 +1,4 @@
-package diosantander.banklineapi;
+package diosantanderbankline.banklineapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
